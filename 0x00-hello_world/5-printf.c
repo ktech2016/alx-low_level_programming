2c39cd92c7082b937bfa567454f3c71a6e284465
olueesuola@gmail.com <ktech2016>
2022-09-10 07:20:58 -0700

    update on 5-print.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,6 @@
 
 /**
 * main - print the string in the put function
-*
 * Description: using the main function
 * this program prints "Programming is like abuilding a multilingual puzzle
 * Return: 0
@@ -10,5 +9,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
