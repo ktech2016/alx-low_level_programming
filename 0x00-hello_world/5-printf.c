1b2f20fa22f808961cdaef966ce7f4d2cd9f0b80
olueesuola@gmail.com <ktech2016>
2022-09-10 07:44:20 -0700

    more update on task 5 update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,11 +7,6 @@
 */
 int main(void)
 {
-<<<<<<< HEAD
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-=======
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
->>>>>>> 9b36a277d71276ef27d2e3bc69ce5c5c02f50777
+return (0);
 }
