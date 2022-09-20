c31739e0ea0c500bb138e2edfbb96106b90f0296
olueesuola@gmail.com <ktech2016>
2022-09-19 17:18:09 -0700

    fix on task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,22 +1,43 @@
-#include "holberton.h"
-/**
- * puts_half - Prints out the first half of a string.
- * @str: input string to print.
- */
-void puts_half(char *str)
-{
-	int i, j;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-	while (str[i] != '\0')
-		i++;
-	if (i % 2 == 0)
-		j = i / 2;
-	else
-		j = (i + 1) / 2;
-	while (j < i)
+/**
+ * main - random password generator for 101-crackme
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
+
+	srand(time(NULL));
+	while (s != 2772)
 	{
-		_putchar(str[j]);
-		j++;
+		i = k = s = 0;
+		while ((2772 - 122) > s)
+		{
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
+		}
+		while (c[k])
+		{
+			if (c[k] == (2772 - s))
+			{
+				p[i] = c[k];
+				s += c[k];
+				i++;
+				break;
+			}
+			k++;
+		}
 	}
-	_putchar('\n');
+	p[i] = '\0';
+	printf("%s", p);
+	return (0);
 }
+
