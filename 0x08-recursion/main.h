2ac4ef4eb4eb8d48db994d17666b97fc6d8297bc
olueesuola@gmail.com <ktech2016>
2022-09-28 18:51:27 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,19 +1,24 @@
-#include "main.h"
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-/**
- * wildcmp - compares two strings and returns 1 if identical
- * @s1: string to be checked
- * @s2: pattern to be used
- *
- * Return: 1 if identical, 0 otherwise
- */
-int wildcmp(char *s1, char *s2)
-{
-	if (*s2 == '\0')
-		return (*s1 == '\0');
-	if (*s2 == *s1)
-		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
-	return (0);
-}
+void _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+
+int is_prime_number(int n);
+
+int is_palindrome(char *s);
+
+int wildcmp(char *s1, char *s2);
+
+#endif
